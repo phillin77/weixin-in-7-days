8eb1c99698169d9746fdb5deb448e15fb564926d
phillin77 <phillin77@gmail.com>
2017-08-07 23:21:44 +0800

    ch1.6 - 2: 完成微信驗證

--- app.js
+++ app.js
@@ -24,6 +24,22 @@ var app = new Koa()
 
 app.use(function* (next) {
 	console.log(this.query)
+
+	var token = config.wechat.token
+	var signature = this.query.signature
+	var nonce = this.query.nonce
+	var timestamp = this.query.timestamp
+	var echostr = this.query.echostr
+	var str = [token, timestamp, nonce].sort().join('')
+	var sha = sha1(str)
+
+	if (sha === signature) {
+		this.body = echostr + ''
+	}
+	else {
+		this.body = 'wrong'
+	}
+
 }) // app.use
 
 app.listen(server_port)
