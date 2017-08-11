d98def55da3b050cd5875290ad91800244739207
phillin77 <phillin77@gmail.com>
2017-08-11 16:37:42 +0800

    ch5.8 - 1: 測試 JS-SDK 接口的簡易實作

--- app.js
+++ app.js
@@ -18,6 +18,25 @@ const server_port = process.env.WECHAT_PORT || 1234
 
 var app = new Koa()
 
+//----------------------------------------------------
+// 測試 JS-SDK 的接口用的簡易版 route 實作
+// Note: 如果不使用 JS-SDK，則不須這個 middleware 的 usr
+
+app.use(function *(next) {
+	// 如果網址中有特殊字串，如 (/movie)
+	if (this.url.indexOf('/movie') > -1) {
+		this.body = '<hi>Hi JS-SDK'
+
+		return next  // 處理完畢，不再往下走其他的 middleware，返回頁面結果
+	}
+
+	yield next
+})
+
+// end of 測試 JS-SDK 的接口用的簡易版 route 實作
+//----------------------------------------------------
+
+// 引入 微信 實作的中間件 (middleware)
 app.use(wechat(config.wechat, reply.reply))
 
 app.listen(server_port)
